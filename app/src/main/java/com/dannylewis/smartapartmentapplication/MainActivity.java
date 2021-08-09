4930b7f6f77749199065e4692ae3adf714b3e570
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-08-08 22:09:44 -0400

    Initital Commit

--- app/src/main/java/com/dannylewis/smartapartmentapplication/MainActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/MainActivity.java
@@ -2,13 +2,31 @@ package com.dannylewis.smartapartmentapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //This is the splash screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Determine which screen to send user to
+        goSomewhere();
+    }
+
+    void goSomewhere() {
+        //Get shared preferences
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        if (sharedPref.getBoolean("isConnected", false)) {
+            //Go To Dashboard
+
+        }
+        else {
+            //Go To Setup
+        }
     }
 }
