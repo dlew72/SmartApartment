c7502a6e21d702baf8a8f3f8da6844b1206a8e1d
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-08-24 15:43:01 -0400

    good place with dashboard formatting

--- app/src/main/java/com/dannylewis/smartapartmentapplication/DashboardActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/DashboardActivity.java
@@ -3,6 +3,8 @@ package com.dannylewis.smartapartmentapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +12,33 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_calendar) {
+
+            // Do something
+            return true;
+        }
+        if (id == R.id.action_settings) {
+
+            // Do something
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
