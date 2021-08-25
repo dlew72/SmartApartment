525edf81d3644a7fa10f76a15fa0631cef898b0d
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-08-25 12:59:43 -0400

    format dashboard a little cleaner, slightly more work towards settings dash

--- app/src/main/java/com/dannylewis/smartapartmentapplication/SettingsDashActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/SettingsDashActivity.java
@@ -3,6 +3,7 @@ package com.dannylewis.smartapartmentapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsDashActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SettingsDashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_dash);
+    }
+
+    public void goToLightSettings(View view) {
     }
 }
