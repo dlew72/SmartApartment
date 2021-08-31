449d8f99539750e267383a2990649933703274f4
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-08-31 17:13:18 -0400

    save progress

--- app/src/main/java/com/dannylewis/smartapartmentapplication/SchedulerDashActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/SchedulerDashActivity.java
@@ -3,6 +3,7 @@ package com.dannylewis.smartapartmentapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SchedulerDashActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class SchedulerDashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scheduler_dash);
+
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+    }
+
+    public void goToShadeScheduler(View view) {
+
+    }
+
+    public void goToLightScheduler(View view) {
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
