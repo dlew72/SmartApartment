1a0cbd1641a9131829d1a2655d22aee99842c708
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-09-19 23:02:12 -0400

    start of http smarthub communication

---
+++ Smart_Hub_Code/Smart_Hub_Code.ino
@@ -0,0 +1,55 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h> 
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>   // Include the WebServer library
+
+ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+void handleRoot();              // function prototypes for HTTP handlers
+void handleNotFound();
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  Serial.print("Setting soft-AP ... ");
+  boolean result = WiFi.softAP("ECE Smart Hub", "seniordesign");
+  if(result == true)
+  {
+    Serial.println("Ready");
+    if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
+
+  server.begin();                           // Actually start the server
+  Serial.println("HTTP server started");
+    Serial.println("Server started");
+
+  }
+  else
+  {
+    Serial.println("Failed!");
+  }
+}
+
+void loop()
+{
+  server.handleClient();
+}
+
+void handleRoot() {
+  Serial.println("ROOT");
+  server.send(200, "text/plain", "Hello world 123!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
+}
+
+void handleNotFound(){
+    Serial.println("NOT FOUND");
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
--- app/src/main/java/com/dannylewis/smartapartmentapplication/SmartHubConnectedActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/SmartHubConnectedActivity.java
@@ -2,9 +2,24 @@ package com.dannylewis.smartapartmentapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class SmartHubConnectedActivity extends AppCompatActivity {
 
@@ -22,5 +37,48 @@ public class SmartHubConnectedActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         //Do nothing if back button is pressed on this screen
+    }
+
+    public void sendCredentials(View view) {
+        TextView displayData = findViewById(R.id.dispData);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url ="http://" + getApIpAddr(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url+"/",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        displayData.setText("Response is: "+ response);
+                    }
+                }, (Response.ErrorListener) error -> displayData.setText("That didn't work! +" + error));
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public static String getApIpAddr(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+        byte[] ipAddress = convert2Bytes(dhcpInfo.serverAddress);
+        try {
+            String apIpAddr = InetAddress.getByAddress(ipAddress).getHostAddress();
+            return apIpAddr;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static byte[] convert2Bytes(int hostAddress) {
+        byte[] addressBytes = { (byte)(0xff & hostAddress),
+                (byte)(0xff & (hostAddress >> 8)),
+                (byte)(0xff & (hostAddress >> 16)),
+                (byte)(0xff & (hostAddress >> 24)) };
+        return addressBytes;
     }
 }
