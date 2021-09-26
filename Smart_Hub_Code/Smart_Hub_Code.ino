d01cab80d24e519e85ec27fa3aeba210ac3a28a6
dlew72 <51759034+dlew72@users.noreply.github.com>
2021-09-26 17:59:45 -0400

    sends creds

--- Smart_Hub_Code/Smart_Hub_Code.ino
+++ Smart_Hub_Code/Smart_Hub_Code.ino
@@ -3,40 +3,93 @@
 #include <ESP8266WiFiMulti.h> 
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>   // Include the WebServer library
+#include <EEPROM.h>
 
 ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
 
+int addr = 0;
+bool operationMode = false;
+
 void handleRoot();              // function prototypes for HTTP handlers
 void handleNotFound();
+void(* resetFunc) (void) = 0;
+
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
 
-  Serial.print("Setting soft-AP ... ");
-  boolean result = WiFi.softAP("ECE Smart Hub", "seniordesign");
-  if(result == true)
-  {
-    Serial.println("Ready");
-    if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
-    Serial.println("mDNS responder started");
-  } else {
-    Serial.println("Error setting up MDNS responder!");
+  if (EEPROM.read(0) == char(7)) {
+    //Supposedly set up. Try connecting
+    String ssid = "";
+    String password = "";
+    int ssidLen = EEPROM.read(1);
+    int passwordLen = EEPROM.read(2+ssidLen);
+
+    for (int i = 0; i < ssidLen; i++) {
+      ssid += EEPROM.read(2+i);
+    }
+
+    for (int i = 0; i < passwordLen; i++) {
+      password += EEPROM.read(2+i);
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    unsigned long timeOfStartConnection = millis();
+
+    
+    while (1) {
+      if (WiFi.status() == WL_CONNECTED) {
+        //We connected to the router
+        operationMode = true;
+        break;
+      }
+      if (millis() - timeOfStartConnection > 10000) {
+        //connection failed (timeout)
+        break;
+      }
+      //TODO: blink red light
+    }
+
+    if (!operationMode) {
+      Serial.println("Connection failed. Trying again...");
+      loopAttemptConnection();
+    }
+
+  }
+  else {
+    //No saved credentials. Enter setup mode
+    hubSetupMode();
   }
 
-  server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/", handleRoot);                      // Call the 'handleRoot' function when a client requests URI "/"
+  server.on("/credentials", handleCredentials);    // Call the 'handleRoot' function when a client requests URI "/credentials"
+  server.on("/schedule", handleCredentials);       // Call the 'handleRoot' function when a client requests URI "/schedule"
+  server.on("/action", handleCredentials);         // Call the 'handleRoot' function when a client requests URI "/action"
+
+
   server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
 
   server.begin();                           // Actually start the server
   Serial.println("HTTP server started");
-    Serial.println("Server started");
+}
 
+void hubSetupMode() {
+    Serial.print("Setting soft-AP ... ");
+    boolean result = WiFi.softAP("ECE Smart Hub", "seniordesign");
+    if(result == true)
+    {
+      Serial.println("Ready");
+      if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+      Serial.println("mDNS responder started");
+    } else {
+      Serial.println("Error setting up MDNS responder!");
+    }
   }
   else
-  {
-    Serial.println("Failed!");
-  }
+    Serial.println("Failure to set up Soft-AP");
 }
 
 void loop()
@@ -49,7 +102,90 @@ void handleRoot() {
   server.send(200, "text/plain", "Hello world 123!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
 }
 
+void handleCredentials() {
+  Serial.println("CREDENTIALS");
+  if (server.hasArg("plain")== false){ //Check if body received
+         server.send(400, "text/plain", "fail");
+         return;
+      }
+      
+      server.send(200, "text/plain", "success");
+
+      /** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+      addr = 0;
+      EEPROM.write(addr, char(7));
+      addr++;
+      
+      String message = server.arg("plain");
+      
+      String username = message.substring(9, message.indexOf("\",\"p$$$\":\""));
+
+      String password = message.substring(message.indexOf("p$$$")+7, message.length()-2);
+
+      EEPROM.write(addr, username.length());
+      addr++;
+
+      for (int i = 0; i < username.length(); i++) {
+         EEPROM.write(addr, username[i]);
+         addr++;
+      }
+
+      EEPROM.write(addr, password.length());
+      addr++;
+
+      for (int i = 0; i < password.length(); i++) {
+         EEPROM.write(addr, password[i]);
+         addr++;
+      }
+
+      //Try to use credentials to connect:
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(username, password);
+      unsigned long timeOfStartConnection = millis();
+  
+      while (1) {
+        if (WiFi.status() == WL_CONNECTED) {
+          //We connected to the router
+          operationMode = true;
+          break;
+        }
+        if (millis() - timeOfStartConnection > 10000) {
+          //connection failed (timeout)
+          break;
+        }
+        //TODO: blink red light
+      }
+
+      if (!operationMode) {
+        //credentials failed. wipe EEPROM(0) and reset.
+        EEPROM.write(0, 0);
+        Serial.println("RESETTING...");
+        resetFunc();
+      }
+      
+ 
+}
+
 void handleNotFound(){
     Serial.println("NOT FOUND");
   server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void loopAttemptConnection() {
+   while(1) {
+      if (WiFi.status() == WL_CONNECTED) {
+        //We connected to the router
+        operationMode = true;
+        break;
+      }
+      else {
+        delay(500);
+        Serial.println("Connection failed. Trying again...");
+      }
+   }
+}
+
+void clearSavedCredentials() {
+  for (int i = 0; i < 100; i++)
+    EEPROM.write(i, 0);
 }
--- app/src/main/java/com/dannylewis/smartapartmentapplication/SmartHubConnectedActivity.java
+++ app/src/main/java/com/dannylewis/smartapartmentapplication/SmartHubConnectedActivity.java
@@ -8,20 +8,33 @@ import android.content.Intent;
 import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class SmartHubConnectedActivity extends AppCompatActivity {
+
+    private EditText usern;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,25 +52,62 @@ public class SmartHubConnectedActivity extends AppCompatActivity {
         //Do nothing if back button is pressed on this screen
     }
 
-    public void sendCredentials(View view) {
+    public void sendCredentials(View view) throws JSONException {
         TextView displayData = findViewById(R.id.dispData);
+
+        usern = findViewById(R.id.networkName);
+        pass = findViewById(R.id.passwordNETWORK);
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
         String url ="http://" + getApIpAddr(this);
 
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("u###", usern.getText());
+        jsonBody.put("p$$$", pass.getText());
+        final String requestBody = jsonBody.toString();
+
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url+"/",
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url+"/credentials",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        displayData.setText("Response is: "+ response);
+                        Log.i("VOLLEY", response);
                     }
-                }, (Response.ErrorListener) error -> displayData.setText("That didn't work! +" + error));
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
 
-// Add the request to the RequestQueue.
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    responseString = String.valueOf(response.statusCode);
+                    displayData.setText(response.statusCode);
+                    // can get more details such as response.headers
+                }
+                return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+            }
+        };
+        // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }
 
